2c364b4584076a252c9def3a4c7d0f86b29a704e
Shirley Yu <shirleyxy.yu@gmail.com>
2025-10-03 13:40:48 -0400

    All vendi location changed.

--- submit_scripts/submit_all_qvendi_q_sweep.sh
+++ submit_scripts/submit_all_qvendi_q_sweep.sh
@@ -6,19 +6,19 @@ echo "Submitting Q-Vendi q-sweep experiments..."
 
 # submit CIFAR experiments
 echo "Submitting CIFAR experiments..."
-sbatch submit_scripts/submit_cifar_qvendi_q_sweep.sh
+sbatch ./submit_cifar_qvendi_q_sweep.sh
 
 # submit CIFAR PRV experiments
 echo "Submitting CIFAR PRV experiments..."
-sbatch submit_scripts/submit_cifar_prv_qvendi_q_sweep.sh
+sbatch ./submit_cifar_prv_qvendi_q_sweep.sh
 
 # submit MNIST experiments
 echo "Submitting MNIST experiments..."
-sbatch submit_scripts/submit_mnist_qvendi_q_sweep.sh
+sbatch ./submit_mnist_qvendi_q_sweep.sh
 
 # submit MNIST PRV experiments
 echo "Submitting MNIST PRV experiments..."
-sbatch submit_scripts/submit_mnist_prv_qvendi_q_sweep.sh
+sbatch ./submit_mnist_prv_qvendi_q_sweep.sh
 
 echo "All jobs submitted!"
 echo "Check job status with: squeue -u \$USER"
